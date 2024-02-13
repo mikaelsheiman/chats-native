c99b2f8554e0b06ed75e5d8b174caa73cd2db7b0
YarikMix <63838221+mikaelsheiman@users.noreply.github.com>
2024-02-13 22:30:36 +0300

    Clear

--- API/index.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-import {DOMEN} from "../consts";
-
-export const axiosInstance = axios.create({ baseURL: `http://${DOMEN}/api` });
--- consts.js
+++
@@ -1,1 +0,0 @@
-export const DOMEN = "192.168.1.69:8000"
